bbd6449d71bd8812f27dfb7bff55b6ab2d9d32f9
Shenanigamii <159212161+Shenanigamii@users.noreply.github.com>
2024-02-14 04:25:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+Start # starts the program
+  input myNumber # allows the user to enter a value
+  myAnswer = myNumber * 10 # multiplies the value by 10
+  output myAnswer # outputs the result
+Stop # stops the program
